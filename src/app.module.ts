dd2415bce075d26736d0d05b15823eb49ab5957d
César Flores Carrillo <cesarin_gfc@hotmail.es>
2021-12-22 17:59:43 -0700

    credentials

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
-
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './collections/users/users.module';
 import { RolesModule } from './collections/roles/roles.module';
 import { RolesGuard } from './core/guards/roles.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { NotesModule } from './collections/notes/notes.module';
-
+require('dotenv').config();
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://karenojeda:Marissami1@reparacionesletys.qayop.mongodb.net/LetysDB', {
+    MongooseModule.forRoot(process.env.MONGOENV, {
       useNewUrlParser: true
     }),
     AuthModule,
